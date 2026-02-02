20176cf0779beb8886e0d0c4879dae57d6c962f5
aabagdi <70984836+aabagdi@users.noreply.github.com>
2026-02-02 00:46:07 -0800

    initial commit

---
+++ CodeMan/CodeManApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeManApp.swift
+//  CodeMan
+//
+//  Created by Aadit Bagdi on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeManApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ CodeMan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeMan
+//
+//  Created by Aadit Bagdi on 2/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
