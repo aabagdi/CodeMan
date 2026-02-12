a4fce681820f00546c93e4b538e26b4192621bb3
aabagdi <70984836+aabagdi@users.noreply.github.com>
2026-02-12 13:58:25 -0800

    added more security considerations to PythonRunner, like setup/user code namespace seperation and module whitelist

--- CodeMan/PythonRunner.swift
+++ CodeMan/PythonRunner.swift
@@ -62,48 +62,37 @@ final class PythonRunner {
       )
     }
     
-    guard let freshGlobals = PyDict_New() else {
-      return ExecutionResult(output: "Could not create namespace", isError: true)
+    guard let setupGlobals = PyDict_New() else {
+      return ExecutionResult(output: "Could not create setup namespace", isError: true)
     }
-    defer { Py_DecRef(freshGlobals) }
+    defer { Py_DecRef(setupGlobals) }
     
-    if let builtins = PyEval_GetBuiltins() {
-      PyDict_SetItemString(freshGlobals, "__builtins__", builtins)
+    guard let userGlobals = PyDict_New() else {
+      return ExecutionResult(output: "Could not create user namespace", isError: true)
     }
+    defer { Py_DecRef(userGlobals) }
     
-    if let nameObj = PyUnicode_FromString("__main__") {
-      PyDict_SetItemString(freshGlobals, "__name__", nameObj)
-      Py_DecRef(nameObj)
-    }
+    PyDict_SetItemString(setupGlobals, "_user_globals", userGlobals)
     
     guard let codeObj = PyUnicode_FromString(code) else {
       return ExecutionResult(output: "Could not create code string", isError: true)
     }
-    PyDict_SetItemString(freshGlobals, "_user_code", codeObj)
+    PyDict_SetItemString(setupGlobals, "_user_code", codeObj)
     Py_DecRef(codeObj)
     
-    PyErr_Clear()
+    if let builtins = PyEval_GetBuiltins() {
+      PyDict_SetItemString(setupGlobals, "__builtins__", builtins)
+    }
     
-    PyDict_SetItemString(freshGlobals, "_fresh_ns", freshGlobals)
+    PyErr_Clear()
     
     let setupCode = """
     import sys
     import linecache
-    import builtins
     import time as _time
     from io import StringIO
     
-    # Security: Execution timeout
-    _start_time = _time.time()
     _timeout_seconds = \(executionTimeoutSeconds)
-    
-    class _TimeoutError(Exception):
-        pass
-    
-    def _timeout_trace(frame, event, arg):
-        if _time.time() - _start_time > _timeout_seconds:
-            raise _TimeoutError(f"Execution exceeded {_timeout_seconds} second time limit")
-        return _timeout_trace
     
     _user_filename = "<user_code>"
     
@@ -115,31 +104,105 @@ final class PythonRunner {
         _user_filename
     )
     
-    _stdout_capture = StringIO()
-    _stderr_capture = StringIO()
-    _old_stdout = sys.stdout
-    _old_stderr = sys.stderr
-    sys.stdout = _stdout_capture
-    sys.stderr = _stderr_capture
+    _safe_builtins = {
+        # Math & Numbers
+        'abs': abs,
+        'round': round,
+        'pow': pow,
+        'divmod': divmod,
+        'min': min,
+        'max': max,
+        'sum': sum,
+        'int': int,
+        'float': float,
+        'complex': complex,
+        'bool': bool,
+        'bin': bin,
+        'oct': oct,
+        'hex': hex,
+        
+        # Strings & Characters
+        'str': str,
+        'repr': repr,
+        'format': format,
+        'chr': chr,
+        'ord': ord,
+        'ascii': ascii,
+        
+        # Collections
+        'list': list,
+        'tuple': tuple,
+        'dict': dict,
+        'set': set,
+        'frozenset': frozenset,
+        'len': len,
+        'sorted': sorted,
+        'reversed': reversed,
+        'slice': slice,
+        'range': range,
     
-    _fresh_ns['_exec_error'] = None
-    _fresh_ns['_captured_stdout'] = ""
-    _fresh_ns['_captured_stderr'] = ""
-    try:
-        _compiled = compile(_user_code, _user_filename, 'exec')
-        sys.settrace(_timeout_trace)
-        exec(_compiled, _fresh_ns)
-    except _TimeoutError as e:
-        _fresh_ns['_exec_error'] = str(e)
-    except Exception as e:
-        import traceback
-        _fresh_ns['_exec_error'] = ''.join(traceback.format_exception(type(e), e, e.__traceback__))
-    finally:
-        sys.settrace(None)
-        sys.stdout = _old_stdout
-        sys.stderr = _old_stderr
-        _fresh_ns['_captured_stdout'] = _stdout_capture.getvalue()
-        _fresh_ns['_captured_stderr'] = _stderr_capture.getvalue()
+        # Classes
+        '__build_class__': __build_class__,
+        
+        # Iteration
+        'iter': iter,
+        'next': next,
+        'enumerate': enumerate,
+        'zip': zip,
+        'map': map,
+        'filter': filter,
+        'any': any,
+        'all': all,
+        
+        # I/O
+        'print': print,
+        
+        # Constants
+        'True': True,
+        'False': False,
+        'None': None,
+    }
+    
+    # Allowed modules whitelist
+    _allowed_modules = {
+        # Math & Science
+        'math', 'cmath', 'decimal', 'fractions', 'random', 'statistics',
+        
+        # Data Structures
+        'collections', 'heapq', 'bisect', 'array', 'itertools', 'functools',
+        
+        # String & Text
+        'string', 're', 'textwrap',
+        
+        # Type Hints
+        'typing', 'types',
+        
+        # Date & Time
+        'datetime', 'calendar',
+        
+        # Data Formats
+        'json', 'csv',
+        
+        # Other Safe Modules
+        'copy', 'pprint', 'enum', 'dataclasses',
+    }
+    
+    _real_import = __import__
+    
+    def _safe_import(name, globals=None, locals=None, fromlist=(), level=0):
+        top_level = name.split('.')[0]
+        if top_level not in _allowed_modules:
+            raise ImportError(f"Module '{name}' is not allowed. Allowed modules: {', '.join(sorted(_allowed_modules))}")
+        return _real_import(name, globals, locals, fromlist, level)
+    
+    _safe_builtins['__import__'] = _safe_import
+    
+    # Configure user namespace with restricted builtins
+    _user_globals['__builtins__'] = _safe_builtins
+    _user_globals['__name__'] = '__main__'
+    
+    # Compile user code
+    _compiled_code = compile(_user_code, _user_filename, 'exec')
     """
     
     guard let setupCodeObj = Py_CompileString(setupCode, "<setup>", Py_file_input) else {
@@ -148,16 +211,85 @@ final class PythonRunner {
     }
     defer { Py_DecRef(setupCodeObj) }
     
-    let result = PyEval_EvalCode(setupCodeObj, freshGlobals, freshGlobals)
-    if result != nil {
-      Py_DecRef(result)
+    let setupResult = PyEval_EvalCode(setupCodeObj, setupGlobals, setupGlobals)
+    if setupResult != nil {
+      Py_DecRef(setupResult)
     } else if PyErr_Occurred() != nil {
       var pType: UnsafeMutablePointer<PyObject>?
       var pValue: UnsafeMutablePointer<PyObject>?
       var pTraceback: UnsafeMutablePointer<PyObject>?
       PyErr_Fetch(&pType, &pValue, &pTraceback)
       
-      var errorMessage = "Setup code failed"
+      var errorMessage = "Setup failed"
+      if let pValue = pValue, let strObj = PyObject_Str(pValue) {
+        var size: Int = 0
+        if let strPtr = PyUnicode_AsUTF8AndSize(strObj, &size) {
+          errorMessage = String(cString: strPtr)
+        }
+        Py_DecRef(strObj)
+      }
+      
+      if let t = pType { Py_DecRef(t) }
+      if let v = pValue { Py_DecRef(v) }
+      if let tb = pTraceback { Py_DecRef(tb) }
+      
+      return ExecutionResult(output: "Internal error: \(errorMessage)", isError: true)
+    }
+    
+    let executeCode = """
+    class _TimeoutError(Exception):
+        pass
+    
+    _start_time = _time.time()
+    
+    def _timeout_trace(frame, event, arg):
+        if _time.time() - _start_time > _timeout_seconds:
+            raise _TimeoutError(f"Execution exceeded {_timeout_seconds} second time limit")
+        return _timeout_trace
+    
+    _stdout_capture = StringIO()
+    _stderr_capture = StringIO()
+    _old_stdout = sys.stdout
+    _old_stderr = sys.stderr
+    sys.stdout = _stdout_capture
+    sys.stderr = _stderr_capture
+    
+    _exec_error = None
+    _captured_stdout = ""
+    _captured_stderr = ""
+    
+    try:
+        sys.settrace(_timeout_trace)
+        exec(_compiled_code, _user_globals)
+    except _TimeoutError as e:
+        _exec_error = str(e)
+    except Exception as e:
+        import traceback
+        _exec_error = ''.join(traceback.format_exception(type(e), e, e.__traceback__))
+    finally:
+        sys.settrace(None)
+        sys.stdout = _old_stdout
+        sys.stderr = _old_stderr
+        _captured_stdout = _stdout_capture.getvalue()
+        _captured_stderr = _stderr_capture.getvalue()
+    """
+    
+    guard let executeCodeObj = Py_CompileString(executeCode, "<execute>", Py_file_input) else {
+      PyErr_Clear()
+      return ExecutionResult(output: "Could not compile execute code", isError: true)
+    }
+    defer { Py_DecRef(executeCodeObj) }
+    
+    let execResult = PyEval_EvalCode(executeCodeObj, setupGlobals, setupGlobals)
+    if execResult != nil {
+      Py_DecRef(execResult)
+    } else if PyErr_Occurred() != nil {
+      var pType: UnsafeMutablePointer<PyObject>?
+      var pValue: UnsafeMutablePointer<PyObject>?
+      var pTraceback: UnsafeMutablePointer<PyObject>?
+      PyErr_Fetch(&pType, &pValue, &pTraceback)
+      
+      var errorMessage = "Execution failed"
       if let pValue = pValue, let strObj = PyObject_Str(pValue) {
         var size: Int = 0
         if let strPtr = PyUnicode_AsUTF8AndSize(strObj, &size) {
@@ -177,21 +309,21 @@ final class PythonRunner {
     var stderrOutput = ""
     var execError = ""
     
-    if let outputObj = PyDict_GetItemString(freshGlobals, "_captured_stdout") {
+    if let outputObj = PyDict_GetItemString(setupGlobals, "_captured_stdout") {
       var size: Int = 0
       if let outputPtr = PyUnicode_AsUTF8AndSize(outputObj, &size) {
         stdoutOutput = String(cString: outputPtr).trimmingCharacters(in: .whitespacesAndNewlines)
       }
     }
     
-    if let errorObj = PyDict_GetItemString(freshGlobals, "_captured_stderr") {
+    if let errorObj = PyDict_GetItemString(setupGlobals, "_captured_stderr") {
       var size: Int = 0
       if let errorPtr = PyUnicode_AsUTF8AndSize(errorObj, &size) {
         stderrOutput = String(cString: errorPtr).trimmingCharacters(in: .whitespacesAndNewlines)
       }
     }
     
-    if let execErrorObj = PyDict_GetItemString(freshGlobals, "_exec_error") {
+    if let execErrorObj = PyDict_GetItemString(setupGlobals, "_exec_error") {
       var size: Int = 0
       if let errorPtr = PyUnicode_AsUTF8AndSize(execErrorObj, &size), size > 0 {
         execError = String(cString: errorPtr).trimmingCharacters(in: .whitespacesAndNewlines)
